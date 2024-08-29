4e06d02c600a69f3a0770eac0d2f7b6f5b303d5b
qf40805 <qf40805@mail.harpercollege.edu>
2024-08-29 14:56:03 -0500

    Update HelloWorld.java

--- src/main/java/com/csc/HelloWorld.java
+++ src/main/java/com/csc/HelloWorld.java
@@ -1,3 +1,10 @@
 package com.csc;
 
+public class HelloWorld
+			{
+			   public static void main(String[] args)
+			   {  
+			      System.out.print("Hello World!\n");
+			   }
+			}
 // Your code goes here!
